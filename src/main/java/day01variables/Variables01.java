a74d07606c45882231029437ea256066b9a0309d
Iryna R <mnops@ukr.net>
2024-04-30 12:58:48 +0100

    01

--- src/main/java/day01variables/Variables01.java
+++ src/main/java/day01variables/Variables01.java
@@ -5,6 +5,33 @@ public class Variables01 { // opening class body
     public static void main (String[] args){
         //Data type nameOfVariable Assignment Operator value ;
           int          age                =               25 ;
+          float num= 12.99F;
+          double priceOfShirt= 12.99;
+         /*
+
+1) integer: It is used for whole numbers.
+The minimum value for integers is -2,147,483,648
+The maximum value for integers is 2,147,483,647
+integer variables use 32 bits of memory   (8 bits = 1 byte)
+2) short: It is used for whole numbers.
+The minimum value for short is -32,768
+The maximum value for short is 32,767
+short variables use 16 bits of memory
+3) byte: It is used for whole numbers.
+The minimum value for byte is -128
+The maximum value for byte is 127
+byte variables use 8 bits of memory
+Q: What data type will you prefer if you need to store the age of university students?
+byte
+4) long: It is used for whole numbers.
+The minimum value for long -9,223,372,036,854,755,808
+The maximum value for long 9,223,372,036,854,755,807
+long variables use 64 bits of memory
+ */
+int populationOfUK = 67000000;
+
+System.out.println(populationOfUK);
+
     }
 
 
