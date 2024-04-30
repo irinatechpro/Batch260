2dc878c24060b80091f1b0644f6d6e061ee035fb
Iryna R <mnops@ukr.net>
2024-04-30 11:28:11 +0100

    Initial commit

---
+++ src/main/java/day01variables/Variables01.java
@@ -0,0 +1,11 @@
+package day01variables;
+
+public class Variables01 { // opening class body
+
+    public static void main (String[] args){
+        //Data type nameOfVariable Assignment Operator value ;
+          int          age                =               25 ;
+    }
+
+
+    } //closing class body
